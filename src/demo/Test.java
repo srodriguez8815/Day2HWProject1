f1ca95103337e14759bed3bf0eca0e468fe93729
96sam <96sam@DESKTOP-U2TUVL3.mynetworksettings.com>
2023-04-10 18:32:29 -0400

    Initial Commit

---
+++ src/demo/Test.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Test {
+
+	public static void main (String [] args) {
+		
+	}
+	public void getMethod() {
+		System.out.println("Get Method");
+		
+	}
+	
+	public void getInfo() {
+		System.out.println("Get info");
+	}
+}
